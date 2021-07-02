ff4cdc96590811d37cbbf519629ea3706ef6dff5
Awal Hadi Wijaya <awalhadi006@gmail.com>
2021-07-02 23:16:28 +0700

    Update Praktikum 13

--- lab11_ci/ci4/app/Config/Routes.php
+++ lab11_ci/ci4/app/Config/Routes.php
@@ -41,14 +41,14 @@ $routes->get('/article', 'Page::article');
 $routes->get('/contact', 'contact');
 $routes->get('/artikel', 'Artikel::index');
 $routes->get('/artikel/(:any)', 'Artikel::view/$1');
-$routes->group('admin', function($routes) {
+$routes->group('admin', ['filter' => 'auth'], function($routes) {
 	$routes->get('artikel', 'Artikel::admin_index');
 	$routes->add('artikel/add', 'Artikel::add');
 	$routes->add('artikel/edit/(:any)', 'Artikel::edit/$1');
 	$routes->get('artikel/delete/(:any)', 'Artikel::delete/$1');
 	});
 	
-
+$routes->get('/user/login', 'User::login');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
